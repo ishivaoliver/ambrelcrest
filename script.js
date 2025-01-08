e9699768d9b002bd5f4d0b9d67d3b2a7f1ae930f
Oliver Ambrose Ishiva <146702972+ishivaoliver@users.noreply.github.com>
2025-01-08 23:56:13 +0300

    Updatedscript.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const titleText = "Ambrel Crest Technologies";
-const subtitleText = "Crafting something stay tuned!";
+const subtitleText = "Crafting something amazing!";
 const typingTitle = document.getElementById("typing-title");
 const typingSubtitle = document.getElementById("typing-subtitle");
 
